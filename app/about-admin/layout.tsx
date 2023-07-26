0309df398b3fd36296ad8a4304fadadd0274ebe5
Jhagas <jhagas.205001@mhs.its.ac.id>
2023-07-26 16:48:19 +0700

    license

---
+++ app/about-admin/layout.tsx
@@ -0,0 +1,37 @@
+import Credit from "@/components/credit";
+import Navbar from "@/components/navbar";
+import Redirect from "@/components/redirect";
+import { unique } from "@/lib/utils";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Metadata } from "next";
+import { cookies } from "next/headers";
+
+export const metadata: Metadata = {
+  title: "Tentang Admin | Praktikum Fisika Laboratorium",
+  description:
+    "Bantuan Admin Laman Web Penjadwalan dan Penilaian Praktikum Fisika Laboratorium Departemen Fisika Institut Teknologi Sepuluh Nopember",
+};
+
+export const dynamic = "force-dynamic";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div className="bg-zinc-50 dark:bg-zinc-900 px-4 h-screen transition-colors duration-300">
+      {children}
+    </div>
+  );
+}
--- app/about-admin/page.tsx
+++ app/about-admin/page.tsx
@@ -2,7 +2,7 @@ import { BsPenFill } from "react-icons/bs";
 import { IoLogoGithub } from "react-icons/io5";
 import { SiLinkedin } from "react-icons/si";
 import Image from "next/image";
-import { Metadata } from "next";
+import Link from "next/link";
 
 const tech = [
   {
@@ -22,60 +22,59 @@ const tech = [
   },
 ];
 
-export const metadata: Metadata = {
-    title: "Tentang Admin | Praktikum Fisika Laboratorium",
-    description:
-      "Bantuan Admin Laman Web Penjadwalan dan Penilaian Praktikum Fisika Laboratorium Departemen Fisika Institut Teknologi Sepuluh Nopember",
-  };
-
 export default function About() {
   return (
-    <>
-      <div className="bg-zinc-50 dark:bg-zinc-900 px-4 h-screen transition-colors duration-300">
-        <div className="relative bg-white dark:bg-zinc-800 rounded-2xl p-9 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-xl min-w-min shadow-md">
-          <Image
-            src="/assets/profiles.jpeg"
-            alt="Jhagas's Photo of Himself"
-            width="200"
-            height="200"
-            className="rounded-full shadow-md mx-auto mb-1"
-          />
-          <div className="mt-5">
-            <span className="text-gray-800 dark:text-slate-200">
-              <p className="text-center font-black text-lg mb-1">
-                Jhagas Hana Winaya
-              </p>
-              <p className="text-center mb-2">
-                Website ini dibuat dengan Supabase (Database), Next.js (Backend dan
-                Frontend) serta dengan pustaka Tailwind CSS dan DaisyUI
-              </p>
-              <p className="text-center">
-                Bila ada kendala yang berkaitan dengan website ini, seperti lupa
-                password dan sebagainya. Silahkan hubungi saya melalui WhatsApp
-              </p>
-            </span>
-            <div className="text-gray-600 dark:text-zinc-400 mt-8 block text-sm text-center">
-              <p>Portofolio Saya</p>
-              <div className="flex flex-row w-full justify-center items-center mt-4">
-                {tech.map((item) => {
-                  return (
-                    <a
-                      key={item.name}
-                      href={item.link}
-                      target="_blank"
-                      rel="noreferrer"
-                      title={item.name}
-                      className="relative mx-2 transition duration-200 focus:scale-125 hover:scale-125 hover:text-slate-800 dark:hover:text-zinc-100 focus:text-slate-800"
-                    >
-                      <item.logo size="28px" />
-                    </a>
-                  );
-                })}
-              </div>
-            </div>
+    <div className="relative bg-white dark:bg-zinc-800 rounded-2xl p-9 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-xl min-w-min shadow-md">
+      <Image
+        src="/assets/profiles.jpeg"
+        alt="Jhagas's Photo of Himself"
+        width="200"
+        height="200"
+        className="rounded-full shadow-md mx-auto mb-1"
+      />
+      <div className="mt-5">
+        <span className="text-gray-800 dark:text-slate-200">
+          <p className="text-center font-black text-lg mb-1">
+            Jhagas Hana Winaya
+          </p>
+          <p className="text-center mb-2">
+            Website ini dibuat dengan Supabase (Database), Next.js (Backend dan
+            Frontend) serta dengan pustaka Tailwind CSS dan DaisyUI.
+          </p>
+          <div className="w-full flex justify-center mb-2 gap-2">
+            <Link
+              href="https://github.com/jhagas/web-praktikum-fislab"
+              className="btn btn-sm"
+            >
+              <IoLogoGithub />
+              Open-Source (MIT License)
+            </Link>
+          </div>
+          <p className="text-center">
+            Bila ada kendala yang berkaitan dengan website ini, seperti lupa
+            password dan sebagainya. Silahkan hubungi saya melalui WhatsApp
+          </p>
+        </span>
+        <div className="text-gray-600 dark:text-zinc-400 mt-4 block text-sm text-center">
+          <p>Portofolio Saya</p>
+          <div className="flex flex-row w-full justify-center items-center mt-2">
+            {tech.map((item) => {
+              return (
+                <a
+                  key={item.name}
+                  href={item.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  title={item.name}
+                  className="relative mx-2 transition duration-200 focus:scale-125 hover:scale-125 hover:text-slate-800 dark:hover:text-zinc-100 focus:text-slate-800"
+                >
+                  <item.logo size="28px" />
+                </a>
+              );
+            })}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
